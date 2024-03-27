4ce6f2d2f060865fd97d4577889a9c433a6feced
ajaykumar <ajaykumarkeerthi609@gmail.com>
2024-03-27 23:41:26 +0530

    second program

--- Untitled-1.py
+++ Untitled-1.py
@@ -1,3 +1,4 @@
+#program 1
 list1=[10,501,22,37,100,999,87,351]
 odd_num=[]
 even_num=[]
@@ -9,4 +10,19 @@ for num in list1:
         even_num.append(num)
 print("all even numbers:",even_num)
 print("all odd numbers:",odd_num)
+
+#program 2
+list2=[10,501,22,37,100,999,87,351]
+def prime_num(num):
+    if num <=1:
+        return False
+    for i in range(2, int(num ** 0.5) + 1):
+        if num % i == 0:
+            return False
+    return True
+
+prime_numbers = [num for num in list2 if prime_num(num)]
+
+print("Prime numbers:", prime_numbers)
+
 
