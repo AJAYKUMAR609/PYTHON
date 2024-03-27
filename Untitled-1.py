5bfe3e3072ed3c35431c0e7c7f5b8723b3ed8678
ajaykumar <ajaykumarkeerthi609@gmail.com>
2024-03-27 22:50:05 +0530

    first program

--- Untitled-1.py
+++ Untitled-1.py
@@ -1,3 +1,12 @@
-string=["apple","rabbit","ice creame"]
-newstring=max(string)
-print(newstring)
+list1=[10,501,22,37,100,999,87,351]
+odd_num=[]
+even_num=[]
+for num in list1:
+    if num % 2==1:
+        odd_num.append(num)
+    else:
+        num % 2==0,
+        even_num.append(num)
+print("all even numbers:",even_num)
+print("all odd numbers:",odd_num)
+
