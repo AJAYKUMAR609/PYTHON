acfcdee9fe90864befa7aab2933a941dcfa9b335
ajaykumar <ajaykumarkeerthi609@gmail.com>
2024-03-28 21:03:51 +0530

    task3

--- Untitled-1.py
+++ Untitled-1.py
@@ -1,29 +1,3 @@
-#program 1
-list1=[10,501,22,37,100,999,87,351]
-odd_num=[]
-even_num=[]
-for num in list1:
-    if num % 2==1:
-        odd_num.append(num)
-    else:
-        num % 2==0,
-        even_num.append(num)
-print("all even numbers:",even_num)
-print("all odd numbers:",odd_num)
-
-#program 2
-list2=[10,501,22,37,100,999,87,351]
-def prime_num(num):
-    if num <=1:
-        return False
-    for i in range(2, int(num ** 0.5) + 1):
-        if num % i == 0:
-            return False
-    return True
-
-prime_numbers = [num for num in list2 if prime_num(num)]
-print("Prime numbers:", prime_numbers)
-
 
 #program to add first and last numbers
 
@@ -60,3 +34,71 @@ def create_timestamp_file():
 # Call the function to create the timestamp file
 create_timestamp_file()
 
+# Define the number of rows for the pyramid
+num_rows = 20
+
+# Outer loop for rows
+for i in range(1, num_rows + 1):
+    # Print spaces for left alignment
+    print(" " * (num_rows - i), end="")
+
+    # Inner loop for printing numbers
+    for j in range(1, i + 1):
+        print(j, end=" ")
+
+    # Move to the next line
+    print()
+
+
+def count_vowels(string):
+    # Initialize counters for each vowel
+    count_a = count_e = count_i = count_o = count_u = 0
+
+    # Convert the string to lowercase to handle both upper and lower case vowels
+    string = string.lower()
+
+    # Iterate through each character in the string
+    for char in string:
+        if char == 'a':
+            count_a += 1
+        elif char == 'e':
+            count_e += 1
+        elif char == 'i':
+            count_i += 1
+        elif char == 'o':
+            count_o += 1
+        elif char == 'u':
+            count_u += 1
+
+    # Calculate the total number of vowels
+    total_vowels = count_a + count_e + count_i + count_o + count_u
+
+    # Print the counts of each individual vowel and total number of vowels
+    print("Count of 'A':", count_a)
+    print("Count of 'E':", count_e)
+    print("Count of 'I':", count_i)
+    print("Count of 'O':", count_o)
+    print("Count of 'U':", count_u)
+    print("Total number of vowels:", total_vowels)
+
+
+# Test the function with the given string
+input_string = "guvi geeks network private limited"
+count_vowels(input_string)
+
+
+def remove_vowels(input_string):
+    # string containing all vowels of uppercase and lowercase
+    vowels = "aeiouAEIOU"
+
+    # Use list comprehension to create a new string with vowels removed
+    new_string = ''.join(char for char in input_string if char not in vowels)
+
+    return new_string
+
+
+# Test the function
+input_string = "Hello, World! This is a test string."
+result = remove_vowels(input_string)
+print("Original string:", input_string)
+print("String with vowels removed:", result)
