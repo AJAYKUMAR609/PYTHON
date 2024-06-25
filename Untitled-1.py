0cf1faf45eaa5aabbc1410d09190da09d13e2e03
ajaykumar <ajaykumarkeerthi609@gmail.com>
2024-06-25 12:17:13 +0530

    inital commit

--- Untitled-1.py
+++ Untitled-1.py
@@ -187,4 +187,64 @@ NewCircle = Circle(7)
 print(NewCircle.area())
 print(NewCircle.perimeter())
 
+#task-9 displaying webpage name and current url
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+import os
+from selenium.webdriver.common.by import By
+# Set the path to your chromedriver executable
+chromedriver_path = r"C:\Users\ajay\OneDrive\Desktop\chromedriver.exe"
+os.environ["PATH"] += os.pathsep + os.path.dirname(chromedriver_path)
+
+# Create Chrome options
+chrome_options = Options()
+chrome_options.add_experimental_option("detach", True)
+
+# Initialize the WebDriver
+driver = webdriver.Chrome(options=chrome_options)
+
+driver.get("https://www.saucedemo.com/inventory.html")
+driver.find_element(By.ID,"user-name").send_keys("standard_user")
+driver.find_element(By.ID,"password").send_keys("secret_sauce")
+driver.find_element(By.ID,"login-button").click()
+page_source=driver.page_source
+filename='webpage_task_11.txt'
+with open(filename,'w',
+          encoding='utf-8')as f:
+    f.write(page_source)
+print(f"webpage content saved to '{filename}'.")
+print(driver.title)
+print(driver.current_url)
+
+#task-10
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+import os
+import time
+from selenium.webdriver.common.by import By
+# Set the path to your chromedriver executable
+chromedriver_path = r"C:\Users\ajay\OneDrive\Desktop\chromedriver.exe"
+os.environ["PATH"] += os.pathsep + os.path.dirname(chromedriver_path)
+
+# Create Chrome options
+chrome_options = Options()
+chrome_options.add_experimental_option("detach", True)
+
+# Initialize the WebDriver
+driver = webdriver.Chrome(options=chrome_options)
+driver.get("https://www.cowin.gov.in/")
+driver.find_element(By.XPATH,'//*[@id="navbar"]/div[4]/div/div[1]/div/nav/div[3]/div/ul/li[4]/a').click()
+print(driver.title)
+print(driver.current_url)
+driver.switch_to.window(driver.window_handles[1])
+driver.close()
+driver.find_element(By.XPATH,'//*[@id="navbar"]/div[4]/div/div[1]/div/nav/div[3]/div/ul/li[5]/a').click()
+print(driver.title)
+print(driver.current_url)
+driver.switch_to.window(driver.window_handles[-1])
+driver.close()
+
+
+
+
 
--- sauce.py
+++ sauce.py
@@ -27,3 +27,68 @@ print(driver.title)
 print(driver.current_url)
 
 
+#task20
+
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+import os
+import time
+from selenium.webdriver.common.by import By
+# Set the path to your chromedriver executable
+chromedriver_path = r"C:\Users\ajay\OneDrive\Desktop\chromedriver.exe"
+os.environ["PATH"] += os.pathsep + os.path.dirname(chromedriver_path)
+
+# Create Chrome options
+chrome_options = Options()
+chrome_options.add_experimental_option("detach", True)
+
+# Initialize the WebDriver
+driver = webdriver.Chrome(options=chrome_options)
+driver.get("https://www.cowin.gov.in/")
+driver.find_element(By.XPATH,'//*[@id="navbar"]/div[4]/div/div[1]/div/nav/div[3]/div/ul/li[4]/a').click()
+print(driver.title)
+print(driver.current_url)
+driver.find_element(By.XPATH,'//*[@id="navbar"]/div[4]/div/div[1]/div/nav/div[3]/div/ul/li[5]/a').click()
+print(driver.title)
+print(driver.current_url)
+driver.switch_to.window(driver.window_handles[-1])
+driver.close()
+driver.switch_to.window(driver.window_handles[1])
+driver.close()
+
+#task-labour.gov.in
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+import os
+import time
+from selenium.webdriver.common.by import By
+# Set the path to your chromedriver executable
+chromedriver_path = r"C:\Users\ajay\OneDrive\Desktop\chromedriver.exe"
+os.environ["PATH"] += os.pathsep + os.path.dirname(chromedriver_path)
+
+# Create Chrome options
+chrome_options = Options()
+chrome_options.add_experimental_option("detach", True)
+
+# Initialize the WebDriver
+driver = webdriver.Chrome(options=chrome_options)
+driver.get("https://labour.gov.in/")
+driver.find_element(By.XPATH,'//*[@id="nav"]/li[7]/a').click()
+print(driver.title)
+print(driver.current_url)
+driver.find_element(By.XPATH,'//*[@id="navbar"]/div[4]/div/div[1]/div/nav/div[3]/div/ul/li[5]/a').click()
+print(driver.title)
+print(driver.current_url)
+driver.switch_to.window(driver.window_handles[-1])
+driver.close()
+driver.switch_to.window(driver.window_handles[1])
+driver.close()
+
+
+
+
+
+
+
+
+
